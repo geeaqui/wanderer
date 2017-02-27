2aa15ab620ac2ae9becf6717e0b86fb20e43a4f1
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-27 16:34:14 +0000

    imported ui views to public/index.html and created states/home.html

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -2,6 +2,14 @@ angular
 	.module('wanderers')
 	.controller('tripController', tripController)
 
-function tripController(){
+function tripController(Trips, $stateParams, $state){
 	var self = this;
+	self.all = [];
+
+	self.getTrips = function(){
+		Trips.getAll()
+			.then(function(response){
+				console.log(response.data)
+			})
+	}
 }
