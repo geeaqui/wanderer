503950c7247cd213a4a56e83b35a32f94aebe66f
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-27 13:29:53 +0000

    created a temporary database schema for the destinations.js models

--- models/destination.js
+++ models/destination.js
@@ -1,13 +1,40 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-var DestinationSchema = mongoose.Schema({
-	destination{
-		type: String,
-		required: true,
-		unique: true
-	},
-	date{
-		type:Date,
-		required:true,
-	}
-});
+// var DestinationSchema = mongoose.Schema({
+// 	destination{
+// 		type: String,
+// 		required: true,
+// 		unique: true
+// 	},
+// 	date{
+// 		type:Date,
+// 		required:true,
+// 	}
+// });
+
+module.exports =[
+	  {
+    id: 0,
+    destination: "France",
+    cost: 299.99,
+    date:06/06/2017
+  },
+  {
+  	id: 1,
+    destination: "Germany",
+    cost: 349.99,
+    date:06/06/2017
+  },
+  {
+  	id: 2,
+    destination: "Iceland",
+    cost: 499,
+    date:06/06/2017
+  },
+  {
+  	id: 3,
+    destination: "Greece",
+    cost: 399.99,
+    date:06/06/2017
+  }
+]
