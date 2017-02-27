424cb39c19ba40a899004682573999312b6e2976
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-27 11:52:35 +0000

    added seven rest api functions that redirect request to '/'

---
+++ controllers/destinations.js
@@ -0,0 +1,27 @@
+function indexDestination(req, res){
+	res.redirect('/');
+}
+
+function showDestination(req, res){
+	res.redirect('/');
+}
+
+function editDestination(req, res){
+	res.redirect('/');
+}
+
+function newDestination(req, res){
+	res.redirect('/');
+}
+
+function createDestination(req, res){
+	res.redirect('/');
+}
+
+function updateDestination(req, res){
+	res.redirect('/');
+}
+
+function deleteDestination(req, res){
+	res.redirect('/');
+}
