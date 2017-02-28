1dc85233e3a38092d1f108ad13e6078134e030f8
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-28 15:13:06 +0000

    changed the signup.html form input types attributes

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,6 @@
 var Destination = require('../models/destination');
 var User = require('../models/user');
 
-function indexDestination(req, res){
-	Destination.find({} , function(err, destinations) {
-    if(err) res.status(500).json({error: err.message});
-    // data return so now we can render
-    res.status(200).json(destinations);
-  });
-}
 function createUser (req, res) {
 	console.log(req.body)
 	User.create(req.body, function (err, user) {
@@ -19,25 +12,6 @@ function createUser (req, res) {
 	})
 }
 
-
-
-
-function showDestination(req, res){
-}
-
-function updateDestination(req, res){
-	// res.redirect('/');
-}
-
-function deleteDestination(req, res){
-	// res.redirect('/');
-}
-
-
 module.exports = {
-	create: createUser,
-	index : indexDestination,
-	show : showDestination,
-	update : updateDestination,
-	delete : deleteDestination
+	create: createUser
 }
--- public/js/factories/user.fact.js
+++ public/js/factories/user.fact.js
@@ -5,11 +5,7 @@ angular
 function userFactory(API, $resource) {
 	
     return $resource(API + '/users/:id', { id: '@id' }, { 
-        'getAll': { method: 'GET', isArray: false },
-        'get': { method: 'GET' },
         'create': { method: 'POST'},
-        'delete': { method: 'DELETE'},
-        'update': { method: 'PATCH'}
     })	
 }
 
