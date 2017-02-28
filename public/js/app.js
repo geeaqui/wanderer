fd657226b70b4bfa70659f29412f17c5c17e20d0
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-28 10:14:26 +0000

    added mainRouter function in public/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,17 @@
 angular
 	.module('wanderers',['ui.router'])
+	.config(MainRouter)
+
+function MainRouter($stateProvider, $urlRouterProvider){
+	$stateProvider
+		.state('home',{
+			url:'/',
+			templateUrl:'/states/home.html'
+		})
+		.state('bucket',{
+			url:'/wanderers',
+			templateUrl:'/states/bucket.html'
+		})
+
+	$urlRouterProvider.otherwise('/');
+}
