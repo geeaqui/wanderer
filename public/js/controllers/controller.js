8583ed42001fb4070d2dfd57ac221d834d5eb291
Gerald Aqui <gaqui@spartaglobal.co>
2017-03-02 12:49:30 +0000

    added function to console log inbound data from the thirdparty api

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -4,8 +4,16 @@ angular
 
 function tripController(Trips, $stateParams, $state){
 	var self = this;
+	
+	//Objects Array
 	self.tripQuery = [];
+	
+	//Objects
 	self.quotes = [];
+	self.places = [];
+	
+
+	//
 	self.carrier = [];
 
 	self.getTrips = function(){
@@ -15,13 +23,61 @@ function tripController(Trips, $stateParams, $state){
 				self.tripQuery = response.data;
 				console.log(self.tripQuery);
 				self.quotes = self.tripQuery.Quotes;
-				self.quotesFound = self.quotes.length;
-				for(var i=0;i<self.quotes.length; i++){
-					self.carrier.push(self.quotes[i].InboundLeg);
-				}
-				console.log(self.quotes);
-				console.log(self.carrier[0].CarrierIds);
+				self.places = self.tripQuery.Places;
+				self.carriers = self.tripQuery.Carriers;
+				console.log(self.places);
 
+				self.quotesFound = self.quotes.length;
+
+				console.log(self.tripQuery);
+
+				console.log(self.quotes[0].InboundLeg.DestinationId);
+				console.log(self.places[7].PlaceId);
+
+				for(var i = 0; i<self.quotes.length;i++){
+					for(var j= 0; j < self.places.length; j++){
+						if(self.quotes[i].InboundLeg.DestinationId == self.places[j].PlaceId){
+							console.log("Inbound Departure Date: " + self.quotes[i].InboundLeg.DepartureDate);
+							for(var k=0; k< self.carriers.length; k++){
+
+								if(self.quotes[i].InboundLeg.CarrierIds[0] == self.carriers[k].CarrierId){
+									console.log("Inbound Airline: " + self.carriers[k].Name);
+								}
+							}
+							console.log("Inbound Price: " + self.quotes[i].MinPrice);
+							self.destinationName = self.places[j].Name;
+							console.log("Inbound Destination: " + self.destinationName);
+						}else{
+							console.log("this does not match");
+					}
+					}
+				}
 			})
 	}
 } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
