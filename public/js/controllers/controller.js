c2becd71c97377fc1f5d4939904a9be4554bd00f
Gerald Aqui <gaqui@spartaglobal.co>
2017-03-02 15:35:23 +0000

    added if statement to get flight origin for inbound flights data

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -30,30 +30,55 @@ function tripController(Trips, $stateParams, $state){
 				self.quotesFound = self.quotes.length;
 
 				console.log(self.tripQuery);
-
-				console.log(self.quotes[0].InboundLeg.DestinationId);
-				console.log(self.places[7].PlaceId);
+				for(var i=0; i < self.quotes.length; i++){
+					for(var j =0; j<self.places.length;j++){
+						// if(self.quotes[i].InboundLeg.OriginId == self.places[j].PlaceId){
+						// 	console.log("congrats you have found origin");
+						// }
+					}
+				}
 
 				for(var i = 0; i<self.quotes.length;i++){
 					for(var j= 0; j < self.places.length; j++){
+						if(self.quotes[i].InboundLeg.OriginId == self.places[j].PlaceId){
+							self.inboundOrigin = self.places[j].Name;
+							console.log('Inbound Origin: ' + self.inboundOrigin);
+						}
 						if(self.quotes[i].InboundLeg.DestinationId == self.places[j].PlaceId){
-							//inbound data
+							//Departure Date inbound data
 							console.log("Inbound Departure Date: " + self.quotes[i].InboundLeg.DepartureDate);
 							for(var k=0; k< self.carriers.length; k++){
 
 								if(self.quotes[i].InboundLeg.CarrierIds[0] == self.carriers[k].CarrierId){
-									//Inbound data
+									//Inbound Carrier/airline data
 									console.log("Inbound Airline: " + self.carriers[k].Name);
 								}
 							}
-							//inbound flight data
+
+							//Price inbound flight data
 							console.log("Inbound Price: " + self.quotes[i].MinPrice);
-							self.destinationName = self.places[j].Name;
-							//inbound flight data
-							console.log("Inbound Destination: " + self.destinationName);
+							self.inboundDestination = self.places[j].Name;
+
+							//Destination inbound flight data
+							console.log("Inbound Destination: " + self.inboundDestination);
 						}else{
 							console.log("this does not match");
-					}
+						}
+
+						//Outbound flight data
+						if(self.quotes[i].OutboundLeg.DestinationId == self.places[j].PlaceId){
+							//Outbound Deaparture Date
+							console.log("Outbound Departure Date: " + self.quotes[i].OutboundLeg.DepartureDate);
+							self.outBoundDestination = self.places[j].Name;
+							//Outbound Flight Destination
+							console.log("OutBound: " + self.outBoundDestination);
+							for(var k=0; k< self.carriers.length; k++){
+								if(self.quotes[i].OutboundLeg.CarrierIds[0] == self.carriers[k].CarrierId){
+									// Outbound Carrier/airline data
+									console.log("Outbound Airline: " + self.carriers[k].Name);
+								}
+							}
+						}
 					}
 				}
 			})
