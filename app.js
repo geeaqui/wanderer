4caea4bb1caa219ba1c89b780ad5f24971fee8eb
Gerald Aqui <gaqui@spartaglobal.co>
2017-03-03 12:47:16 +0000

    added images in the public folders, added airline logo to states/home.html

--- app.js
+++ app.js
@@ -26,47 +26,11 @@ app.set('view engine' , 'ejs');
 app.use("/api", routes);
 
 app.get('/', function(req, res){
+	console.log(req.body);
   res.render('index.html.ejs')
 
 });
  
-/**
- * HOW TO Make an HTTP Call - GET
- */
-// options for GET
-
-// var apiKey = "prtl6749387986743898559646983194";
-// var country ="GB";
-// var currency = "GBP";
-// var destination = "AE";
-// var optionsget = {
-//     host : 'partners.api.skyscanner.net', // here only the domain name
-//     // (no http/https !)
-//     path : '/apiservices/browsequotes/v1.0/'+country+'/'+currency+'/en-US/uk/'+destination+'/anytime/anytime?apikey='+ apiKey, // the rest of the url with parameters if needed
-//     method : 'GET' // do GET
-// };
-
-// // do the GET request
-// var reqGet = http.request(optionsget, function(res) {
-//     console.log("statusCode: ", res.statusCode);
-//     // uncomment it for header details
-// //  console.log("headers: ", res.headers);
- 
-//     res.on('data', function(d) {
-//         console.info('GET result:\n');
-//         process.stdout.write(d);
-//         console.info('\n\nCall completed');
-//     });
- 
-// });
-
-// reqGet.end();
-// reqGet.on('error', function(e) {
-//     console.error(e);
-// });
-
-
-
 //server port
 app.listen(process.env.PORT || 3000, function(){
 	console.log('app is listening at port 3000');
