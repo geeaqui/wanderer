bfa3097b8eb91029855efa4af94af1bb922f48c4
Gerald Aqui <gaqui@spartaglobal.co>
2017-03-05 17:16:27 +0000

    change the style of navigation

--- public/js/index.js
+++ public/js/index.js
@@ -2,5 +2,5 @@ $(function(){
 	  // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
+  $('.collapsible').collapsible();
 })
